e3a8b25a7a8da2f3dd572eab3b2918f01b86e6e6
BaljeetK79 <baljeetk@nait.ca>
2026-09-03 23:42:59 -0400

    Control structure examples

---
+++ DAY_02/Test_Day02/SolnTestStudent/TestControlStructures/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TestControlStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try different control ststements");
+
+            int count = 10;
+            int loopTimes;
+
+            Console.WriteLine("How many times you want to execute the loop?");
+            loopTimes = int.Parse(Console.ReadLine());
+
+            do
+            {
+                // stop condition must be updated
+                count++;
+                Console.WriteLine($"count is : {count}");
+            } while (count < loopTimes);
+
+            for (int i=0; i<= loopTimes; i++)
+            {
+                Console.WriteLine($"count is : {count++}");
+            }
+
+        }
+    }
+}
---
+++ DAY_02/Test_Day02/SolnTestStudent/TestStudent/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Hello, Welcome!");
+// Declarations
+
+string nameStudent ; // name of student
+string courseName;
+int stuID;
+int stuMarks;
+
+string inputValues;
+//bool enrollment;
+
+
+
+
+// Usage 
+Console.WriteLine("Enter your Name: \t");
+nameStudent = Console.ReadLine();
+
+Console.WriteLine("Enter the Course you want to study: \t");
+courseName = Console.ReadLine();
+
+Console.WriteLine("Enter your student ID: \t");
+inputValues = Console.ReadLine();
+stuID = int.Parse(inputValues);
+
+Console.WriteLine("Enter your Marks: \t");
+inputValues = Console.ReadLine();
+stuMarks = int.Parse(inputValues);
+
+if (stuMarks >= 50)
+{
+    Console.WriteLine("You are enrolled for the course: \t");
+}
+else
+{
+    Console.WriteLine("You can not enroll for the course: \t");
+}
+
+Console.WriteLine("Have you enrolled for the new course: \t");
+inputValues = Console.ReadLine();
+if(bool.TryParse(inputValues, out bool enrollment1))
+{
+    Console.WriteLine($" I have enrolled the class: {enrollment1} ");
+}
+else
+{
+    Console.WriteLine("Conversion was not successful");
+}
+
+
+Console.WriteLine(" Welcome : " + nameStudent);
+Console.WriteLine($" I am studying in : {courseName} ");
+Console.WriteLine($" My student ID is : {stuID} ");
+
+
+
