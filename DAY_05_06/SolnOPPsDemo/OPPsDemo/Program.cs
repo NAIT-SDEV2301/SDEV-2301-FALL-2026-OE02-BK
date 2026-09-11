f78e855d2ec88d5e0240f4fec611124ff3175dc2
BaljeetK79 <baljeetk@nait.ca>
2026-09-10 22:03:05 -0400

    OOPsDemo Project Structure posted

---
+++ DAY_05_06/SolnOPPsDemo/OPPsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OPPsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DAY_05_06/SolnOPPsDemo/OPPsDemo/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPPsDemo
+{
+    public class Student
+    {
+        // Fields - private data members
+
+        // properties - help to read/write the private data member
+
+        // Methods - define the behaviour
+    }
+}
