a04c4b379876e16aee1a682dc0c2746b588ea33b
BaljeetK79 <baljeetk@nait.ca>
2026-09-10 22:28:56 -0400

    Created  First instance of student

--- DAY_05_06/SolnOPPsDemo/OPPsDemo/Program.cs
+++ DAY_05_06/SolnOPPsDemo/OPPsDemo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace OPPsDemo
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OPPsDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           //Declarations
+           //
+           
+            string inputVar;
+            string vName;
+            int vAge;
+
+
+            Console.WriteLine("Enter name ");
+            vName = Console.ReadLine();
+
+            Console.WriteLine("Enter age ");
+            inputVar = Console.ReadLine();
+            vAge = int.Parse(inputVar);
+
+            Student stu1 = new Student(vName, vAge);            
+            Console.WriteLine("Demo OPPs Concepts , Fields, properties, methods, constructor, encapsulation");
+
+            // Display Student record
+
+            Console.WriteLine($"Student 1 name is: {stu1.Name}");
+            Console.WriteLine($"Student 1 age is: {stu1.Age}");
         }
     }
 }
--- DAY_05_06/SolnOPPsDemo/OPPsDemo/Student.cs
+++ DAY_05_06/SolnOPPsDemo/OPPsDemo/Student.cs
@@ -7,9 +7,55 @@ namespace OPPsDemo
     public class Student
     {
         // Fields - private data members
+        private string _name;
+        private int _age;
 
         // properties - help to read/write the private data member
 
-        // Methods - define the behaviour
+        public string Name
+        {                //auto implemented property
+
+            get
+            {                   // reading private data member
+                return _name;
+
+            }
+            set
+            {                    // writing private data member
+                _name = value;
+            }
+            // Methods - define the behaviour
+        }
+
+        public int Age
+        {                //auto implemented property
+
+            get
+            {                   // reading private data member
+                return _age;
+
+            }
+            set
+            {                    // writing private data member
+                _age = value;
+            }
+            // Methods - define the behaviour
+        }
+
+
+        // constructor
+
+        public Student( string name, int age )
+        {
+            Name = name;
+            Age = age;
+        }
+
+
     }
+
+
+
+
 }
+
